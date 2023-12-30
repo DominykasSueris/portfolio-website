e065cf011c312b83aa87efc6c9d434479139d7c2
Dominykas Sueris <dominykassueris@gmail.com>
2023-12-30 21:26:03 +0000

    Projects page created

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import Navbar from "../componets/Navbar";
+import ProjectsCard from "../componets/ProjectsCard";
+
+const projectsDataTest = [
+  {
+    id: 1,
+    title: "React",
+    description: "react",
+    background: "black",
+  },
+  {
+    id: 2,
+    title: "Node",
+    description: "node",
+    background: "red",
+  },
+  {
+    id: 3,
+    title: "Type-Script",
+    description: "Type",
+    background: "blue",
+  },
+];
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1> My Projects</h1>
+        <div>
+          {projectsDataTest.map((project) => (
+            <ProjectsCard
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              description={project.description}
+              background={project.background}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
