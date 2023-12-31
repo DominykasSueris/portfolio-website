0917c5b352dd448c6fcdf511b014546e15bb647d
Dominykas Sueris <dominykassueris@gmail.com>
2023-12-31 19:32:16 +0000

    Link element, onHover title and code bracket icon added.

--- app/componets/ProjectsCard.tsx
+++ app/componets/ProjectsCard.tsx
@@ -1,27 +1,42 @@
+import Link from "next/link";
 import React from "react";
 
 interface ProjectsCardProps {
   id: number;
   title: string;
-  description: string;
   background: string;
+  link: string;
 }
 
-const ProjectsCard = ({
-  id,
-  title,
-  description,
-  background,
-}: ProjectsCardProps) => {
+const ProjectsCard = ({ id, title, background, link }: ProjectsCardProps) => {
   return (
-    <div>
+    <div className="group mx-12 relative">
       <div
-        className="h-52 md:h-72"
+        className="xl:h-52 lg:48 md:h-60 h-32 xl:w-80 lg:w-60 w-52 my-2"
         style={{ background: `${background}` }}
       ></div>
-      <div className="text-white">
-        <h5>{title}</h5>
-        <p>{description}</p>
+      <div className="flex flex-col justify-center text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h5 className="flex justify-center hidden group-hover:block lg:text-base text-sm">
+          {title}
+        </h5>
+        <Link href={link}>
+          <svg
+            className="flex justify-center lg:h-12 h-6 w-full text-black hidden group-hover:block"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <polyline points="7 8 3 12 7 16" />
+            <polyline points="17 8 21 12 17 16" />
+            <line x1="14" y1="4" x2="10" y2="20" />
+          </svg>
+        </Link>
       </div>
     </div>
   );
