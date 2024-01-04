94ca5bbe0def6bbf7fa13bd112b90bc13850b918
Dominykas Sueris <dominykassueris@gmail.com>
2024-01-04 22:54:00 +0000

    About page className refactoring

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,24 +9,24 @@ const About = () => {
     <>
       <Navbar />
       <div className="bg-[#e8e9f7] text-xl overflow-hidden">
-        <div className="h-dvh flex flex-col items-center justify-center mx-[20%] lg:mx-[10%] xl:mx-[30%]">
-          <h1 className="md:text-lg lg:text-4xl text-green-500 flex justify-start w-full">
+        <div className="h-dvh flex flex-col items-center justify-center mx-[20%] lg:mx-[10%] xl:mx-[30%] text-xs sm:text-bs md:text-base xl:text-xl">
+          <h1 className="text-lg md:text-lg lg:text-4xl text-green-500 flex justify-start w-full">
             I am Dom,
           </h1>
-          <p className="text-xs sm:text-bs md:text-base xl:text-xl py-3">
+          <p className="py-3">
             I am a highly adaptable, creative, and collaborative professional
             with a background in hospitality, bringing strong communication and
             problem-solving skills to the table. In both my personal and
             professional life, I prioritize curiosity, lifelong learning, and
             building meaningful relationships.
           </p>
-          <p className="text-xs sm:text-bs md:text-base xl:text-xl py-3">
+          <p className="py-3">
             As an enthusiastic web developer, I am dedicated to continual growth
             and eager to contribute my skills to your team. Dynamic team member
             who is passionate about learning and ready to thrive in a
             collaborative environment.
           </p>
-          <p className="text-xs sm:text-bs md:text-base xl:text-xl py-3">
+          <p className="py-3">
             Please do not hesitate to get in touch, I am excited about the
             prospect of discussing how I can contribute to your team.
           </p>
